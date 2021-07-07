882d6eb7cde86c9d78538c21849a60885d1253cb
ColdForeign <tankon46555@gmail.com>
2021-07-07 03:01:11 +0300

    Bug fix. Tested

--- scripts/gamescript.js
+++ scripts/gamescript.js
@@ -1,75 +1,115 @@
-document.querySelector("#start-button").addEventListener("click", LoadGame);
-document.querySelector("#start-button-pc").addEventListener("click", StartGamePC);
+$("#start-button").click(LoadGame);
+$("#start-button-pc").click(StartGamePC);
 
-const winValue = 15;
-var game, gameDices;
-var firstPlayerBox, secondPlayerBox;
-var firstScoreDOM, secondScoreDOM;
-var rollButton;
+$.global = new Object();
+
+$.global.game = 0;
+$.global.gameDices = 0;
+$.global.firstPlayerBox = 0;
+$.global.secondPlayerBox = 0;
+$.global.firstScoreDOM = 0;
+$.global.secondScoreDOM = 0;
+$.global.rollButton = 0;
+const winValue = 3;
 
 function InitPlayes() {
 	game = new Game();
 	gameDices = new GameDices();
 
-	game.FirstPlayer.Name = firstInput.value == "" ? "FirstPlayer": firstInput.value;
-	game.SecondPlayer.Name = secondInput.value == "" ? "SecondPlayer": secondInput.value;
+	game.FirstPlayer.Name = firstInput.val() == "" ? "FirstPlayer": firstInput.val();
+	game.SecondPlayer.Name = secondInput.val() == "" ? "SecondPlayer": secondInput.val();
 
 	if (playWithPC) 
 		game.SecondPlayer.Name = "This computer";
 
 	game.FirstPlayer.Score = 0;
 	game.SecondPlayer.Score = 0;
+
+}
+
+function PlayAgain() {
+	NullifyGame();
+	$("#die-1").attr("class", "die-list even-roll")
+	$("#die-2").attr("class", "die-list odd-roll")
+	game = undefined;
+	gameDices = undefined;
+
+	playWithPC ? StartGamePC() : LoadGame();
+}
+
+function ToMenu() {
+	NullifyGame();
+	$("#start-box").attr("class", "container");
+	$("#game-box").attr("class", "hidden");
+	$("#game-box").css("filter", "");
+	$("#game-box").css("background", "");
+	firstInput.val("");
+	secondInput.val("");
+	$("#die-1").attr("class", "die-list even-roll")
+	$("#die-2").attr("class", "die-list odd-roll")
+	game = undefined;
+	gameDices = undefined;
+
 }
 
 function InitElements() {
-	rollButton = document.getElementById("roll-button");
-	gameDices.Dices = [...document.querySelectorAll(".die-list")];
-	firstPlayerBox = document.querySelector("#first-player");
-	secondPlayerBox = document.querySelector("#second-player");
+	rollButton = $("#roll-button");
+	gameDices.Dices = [...$(".die-list")];
+	firstPlayerBox = $("#first-player");
+	secondPlayerBox = $("#second-player");
+	$("#winner-box").children("#playagain-button").click(PlayAgain);
+	$("#winner-box").children("#tomenu-button").click(ToMenu);
+	// console.log($(".die-list").children("li[data-side='1']"))
 
-	document.querySelector("#start-box").setAttribute("class", "hidden");
-	document.querySelector("#game-box").setAttribute("class", "game-container")
-	game.turnPlayerBox = document.querySelector(".turn-box").querySelector("#turn-text");
-	game.turnPlayerBox.innerHTML = game.FirstPlayer.Name;
+	$("#start-box").attr("class", "hidden");
+	$("#game-box").attr("class", "game-container")
+	game.turnPlayerBox = $("#turn-text");
+	game.turnPlayerBox.html(game.FirstPlayer.Name);
 
-	rollButton.addEventListener("click", StartGame);
+	rollButton.click(StartGame);
 
 	let duration = getDuration(gameDices.Dices[0], true);
-	rollButton.addEventListener("click", function () {
-		rollButton.disabled = true;
+	rollButton.click(function () {
+		rollButton.prop('disabled', true);
 		setTimeout(function() {
-			rollButton.disabled = false;
+			rollButton.prop('disabled', false);
 			if (game.turn) {
-				game.turnPlayerBox.innerHTML = game.FirstPlayer.Name;
-				firstPlayerBox.classList.toggle("red");
-    			secondPlayerBox.classList.toggle("blue");
-    			game.turnPlayerBox.classList.toggle("red-text");
-    			game.turnPlayerBox.setAttribute("class", "red-text")
+				game.turnPlayerBox.html(game.FirstPlayer.Name);
+				firstPlayerBox.toggleClass("red");
+    			secondPlayerBox.toggleClass("blue");
+    			game.turnPlayerBox.toggleClass("red-text");
+    			game.turnPlayerBox.attr("class", "red-text")
 				CheckGame();	
 			}
 			else {
-				game.turnPlayerBox.innerHTML = game.SecondPlayer.Name;
-				firstPlayerBox.classList.toggle("red");
-    			secondPlayerBox.classList.toggle("blue");
-    			game.turnPlayerBox.setAttribute("class", "blue-text")
+				game.turnPlayerBox.html(game.SecondPlayer.Name);
+				firstPlayerBox.toggleClass("red");
+    			secondPlayerBox.toggleClass("blue");
+    			game.turnPlayerBox.attr("class", "blue-text")
+
+				if (playWithPC) {
+					rollButton.prop('disabled', true);
+					setTimeout(function() {
+						rollButton.click();
+					}, 1000);
+				}
 			}
 		}, duration);
 	});
 }
 
 function SetScore(DOM, value) {
-	DOM.innerHTML = value;
+	DOM.html(value);
 }
 
 function AnimScore(DOM, value) {
-	DOM.classList.add("after-visibility");
-	DOM.style.setProperty('--number', value);
+	DOM.addClass("after-visibility");
+	DOM.css('--number', value);
 
-	var animDur = window.getComputedStyle(DOM, ':after').
-		getPropertyValue('animation-duration')
+	var animDur = DOM.css('--animation-duration');
 
 	setTimeout(function() {
-		DOM.classList.remove("after-visibility");
+		DOM.removeClass("after-visibility");
 	}, TimeToInt(animDur));
 	
 }
@@ -81,11 +121,11 @@ function AddScore(DOM, player, number) {
 }
 
 function InitPlayerBoxes() {
-	firstPlayerBox.querySelector("h3").innerHTML = game.FirstPlayer.Name;
-	secondPlayerBox.querySelector("h3").innerHTML = game.SecondPlayer.Name;
+	firstPlayerBox.children("h3").html(game.FirstPlayer.Name);
+	secondPlayerBox.children("h3").html(game.SecondPlayer.Name);
 
-	firstScoreDOM = firstPlayerBox.querySelector("h4");
-	secondScoreDOM = secondPlayerBox.querySelector("h4");
+	firstScoreDOM = firstPlayerBox.children("h4");
+	secondScoreDOM = secondPlayerBox.children("h4");
 
 	SetScore(firstScoreDOM, game.FirstPlayer.Score);
 	SetScore(secondScoreDOM, game.SecondPlayer.Score);
@@ -133,15 +173,40 @@ function CheckGame() {
 	}
 
 	if ((game.FirstPlayer.Score >= winValue) || (game.SecondPlayer.Score >= winValue)) {
-		EndGame();
+		rollButton.prop('disabled', true);
+
+		let winner = "";
+		if (game.FirstPlayer.Score >= winValue && game.FirstPlayer.Score == game.SecondPlayer.Score)
+			winner = "Standoff";
+		else if (game.FirstPlayer.Score >= winValue)
+			winner = game.FirstPlayer.Name;
+		else if (game.SecondPlayer.Score >= winValue)
+			winner = game.SecondPlayer.Name;
+
+		setTimeout(function() {
+			EndGame(winner);
+		}, 1000);
+		
 	}
 }
 
-function EndGame() {
-	rollButton.disabled = true;
+function EndGame(winner) {
+	$("#game-box").css("filter", 'blur(1rem)');
+	$("#game-box").css("background", 'radial-gradient(50% 40% at center, #0000004f, #00000000)');
+
+	$winnerBox = $("#winner-box");
+	$winnerBox.attr("class", "winner-container")
+	$winnerBox.children(winner != "Standoff" ? "#player-box" : "#wintext-box").html(winner);
 
 }
 
+function NullifyGame() {
+	$("#winner-box").attr("class", "hidden");
+	$("#game-box").css("filter", "");
+	$("#game-box").css("background", "");
+	rollButton.prop('disabled', false);
+	rollButton.unbind();
+}
 
 function getDuration(element, transition) {
 	let durString;
